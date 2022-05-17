a7f0fcf41130f00a80d72aa4f057d172437e979b
Artur Braga <61604082+artoop@users.noreply.github.com>
2022-05-17 18:03:15 -0300

    storybook added

---
+++ src/stories/book-detail.component.stories.ts
@@ -0,0 +1,10 @@
+import { BookDetailComponent } from "src/app/book-detail/book-detail.component";
+
+export default {
+    title: 'Book-detail',
+    component: BookDetailComponent
+};
+
+export const Default = () => ({
+    component: BookDetailComponent
+});
---
+++ src/stories/library.component.stories.ts
@@ -0,0 +1,10 @@
+import { LibraryComponent } from "src/app/library/library.component"
+
+export default {
+    title: 'Library',
+    component: LibraryComponent
+};
+
+export const Default = () => ({
+    component: LibraryComponent
+});
